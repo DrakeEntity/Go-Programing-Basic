3235657b060de3c766c85768aaeccff57f272dd2
Deepak Raj <54245038+perfect104@users.noreply.github.com>
2020-01-16 15:11:49 +0530

    intial commit #160120
    Print country capital using map function in Golang

---
+++ Go-Maps.go
@@ -0,0 +1,36 @@
+/* Map is another data type like structure and dictionary(python). A key is an objects that use to
+retrive a value later. */
+
+package main
+
+import "fmt"
+
+func main() {
+	var countryCapitalMap map[string]string
+	/* Create A Map */
+	countryCapitalMap = make(map[string]string)
+
+	/* insert Key and values */
+	countryCapitalMap["France"] = "Paris"
+	countryCapitalMap["Italy"] = "Rome"
+	countryCapitalMap["Japan"] = "Tokyo"
+	countryCapitalMap["India"] = "New Delhi"
+
+	/* print map using key */
+	for country, capital := range countryCapitalMap {
+		fmt.Println("Capital of ", country, "is", capital)
+	}
+	/* Test if the country present in the map or not */
+
+	capital, ok := countryCapitalMap["India"]
+
+	/* if ok is true, entry is presnt */
+	if ok {
+		fmt.Println("___________________________________")
+		fmt.Println("Capital of India is", capital)
+	} else {
+		fmt.Println("___________________________________")
+		fmt.Println("Capital of India is not in the Map")
+	}
+
+}
